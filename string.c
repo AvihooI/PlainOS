90055cc35f926d20ac8f54d308c5ef9e27265860
Avihoo Ilan <AvihooI@Gmail.com>
2017-07-22 03:46:30 +0300

    Used an array of values for hextochar instead of a switch.

--- string.c
+++ string.c
@@ -51,59 +51,12 @@ void strcat(char* dest, const char *source)
 
 char hextochar(byte hex)
 {
-    switch(hex)
-    {
-        case 0:
-            return '0';
-            break;
-        case 1:
-            return '1';
-            break;
-        case 2:
-            return '2';
-            break;
-        case 3:
-            return '3';
-            break;
-        case 4:
-            return '4';
-            break;
-        case 5:
-            return '5';
-            break;
-        case 6:
-            return '6';
-            break;
-        case 7:
-            return '7';
-            break;
-        case 8:
-            return '8';
-            break;
-        case 9:
-            return '9';
-            break;
-        case 10:
-            return 'a';
-            break;
-        case 11:
-            return 'b';
-            break;
-        case 12:
-            return 'c';
-            break;
-        case 13:
-            return 'd';
-            break;
-        case 14:
-            return 'e';
-            break;
-        case 15:
-            return 'f';
-            break;
-        default:
-            return -1;
-    }
+    static const char values[] = "0123456789abcdef";
+
+    if (hex < 16)
+        return values[hex];
+    
+    return -1;
 }
 
 void bytetostr(byte value, char buffer[3])
