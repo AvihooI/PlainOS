b5f84c1569c98f2c00058afbb9133a2b4504261c
Avihoo Ilan <AvihooI@Gmail.com>
2017-07-24 06:16:12 +0300

    Corrected a mistake in type to str in string.c where values wouldn't get converted properly.

--- string.c
+++ string.c
@@ -55,14 +55,14 @@ char hextochar(byte hex)
 
     if (hex < 16)
         return values[hex];
-    
+
     return -1;
 }
 
 void bytetostr(byte value, char buffer[3])
 {
     buffer[2] = '\0';
-    buffer[1] = hextochar(value & 0x00FF);
+    buffer[1] = hextochar(value & 0xF);
     buffer[0] = hextochar(value >> 4);
 }
 
@@ -70,41 +70,41 @@ void wordtostr(word value, char buffer[5])
 {
     buffer[4] = '\0';
     buffer[3] = hextochar(value & 0x00FF);
-    buffer[2] = hextochar((value >> 4) & 0x00FF);
-    buffer[1] = hextochar((value >> 8) & 0x00FF);
-    buffer[0] = hextochar((value >> 12) & 0x00FF);
+    buffer[2] = hextochar((value >> 4) & 0xF);
+    buffer[1] = hextochar((value >> 8) & 0xF);
+    buffer[0] = hextochar((value >> 12) & 0xF);
 }
 
 void dwordtostr(dword value, char buffer[9])
 {
     buffer[8] = '\0';
-    buffer[7] = hextochar(value & 0x00FF);
-    buffer[6] = hextochar((value >> 4) & 0x00FF);
-    buffer[5] = hextochar((value >> 8) & 0x00FF);
-    buffer[4] = hextochar((value >> 12) & 0x00FF);
-    buffer[3] = hextochar((value >> 16) & 0x00FF);
-    buffer[2] = hextochar((value >> 20) & 0x00FF);
-    buffer[1] = hextochar((value >> 24) & 0x00FF);
-    buffer[0] = hextochar((value >> 28) & 0x00FF);
+    buffer[7] = hextochar(value & 0xF);
+    buffer[6] = hextochar((value >> 4) & 0xF);
+    buffer[5] = hextochar((value >> 8) & 0xF);
+    buffer[4] = hextochar((value >> 12) & 0xF);
+    buffer[3] = hextochar((value >> 16) & 0xF);
+    buffer[2] = hextochar((value >> 20) & 0xF);
+    buffer[1] = hextochar((value >> 24) & 0xF);
+    buffer[0] = hextochar((value >> 28) & 0xF);   
 }
 
 void qwordtostr(qword value, char buffer[17])
 {
     buffer[16] = '\0';
-    buffer[15] = hextochar(value & 0x00FF);
-    buffer[14] = hextochar((value >> 4) & 0x00FF);
-    buffer[13] = hextochar((value >> 8) & 0x00FF);
-    buffer[12] = hextochar((value >> 12) & 0x00FF);
-    buffer[11] = hextochar((value >> 16) & 0x00FF);
-    buffer[10] = hextochar((value >> 20) & 0x00FF);
-    buffer[9] = hextochar((value >> 24) & 0x00FF);
-    buffer[8] = hextochar((value >> 28) & 0x00FF);
-    buffer[7] = hextochar((value >> 32) & 0x00FF);
-    buffer[6] = hextochar((value >> 36) & 0x00FF);
-    buffer[5] = hextochar((value >> 40) & 0x00FF);
-    buffer[4] = hextochar((value >> 44) & 0x00FF);
-    buffer[3] = hextochar((value >> 48) & 0x00FF);
-    buffer[2] = hextochar((value >> 52) & 0x00FF);
-    buffer[1] = hextochar((value >> 56) & 0x00FF);
-    buffer[0] = hextochar((value >> 60) & 0x00FF);
+    buffer[15] = hextochar(value & 0xF);
+    buffer[14] = hextochar((value >> 4) & 0xF);
+    buffer[13] = hextochar((value >> 8) & 0xF);
+    buffer[12] = hextochar((value >> 12) & 0xF);
+    buffer[11] = hextochar((value >> 16) & 0xF);
+    buffer[10] = hextochar((value >> 20) & 0xF);
+    buffer[9] = hextochar((value >> 24) & 0xF);
+    buffer[8] = hextochar((value >> 28) & 0xF);
+    buffer[7] = hextochar((value >> 32) & 0xF);
+    buffer[6] = hextochar((value >> 36) & 0xF);
+    buffer[5] = hextochar((value >> 40) & 0xF);
+    buffer[4] = hextochar((value >> 44) & 0xF);
+    buffer[3] = hextochar((value >> 48) & 0xF);
+    buffer[2] = hextochar((value >> 52) & 0xF);
+    buffer[1] = hextochar((value >> 56) & 0xF);
+    buffer[0] = hextochar((value >> 60) & 0xF);
 }
