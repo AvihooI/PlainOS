3df2d1ea38accb623a00d28736671f250eeda900
Avihoo Ilan <AvihooI@Gmail.com>
2017-07-24 06:17:11 +0300

    Added stub.c to contain empty implementations of functions that aren't implemented yet in order to allow linking.

---
+++ stub.c
@@ -0,0 +1,15 @@
+/* Contains empty implementations of functions in order to allow linking */
+#include "types.h"
+
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+void hardware_interrupt_handler(struct regs *r)
+{
+
+}
+
+void user_interrupt_handler(struct regs *r)
+{
+
+}
